9b6c28b6341f541e1a96bb23b1eee4416db7065f
David Grier <david.grier@sds.co.uk>
2024-11-23 13:13:09 +0000

    No 'base' required

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,4 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://indigo-delta.github.io',
-  base: 'indigo-delta',
 });
